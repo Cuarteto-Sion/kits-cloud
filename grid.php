c913f0768a061cd06d1bf507d5cd98b05b710891
Oliver Montalvan Morales <olivermontalvanm@gmail.com>
2022-11-17 22:31:27 -0600

    Added new grid layout

---
+++ grid.php
@@ -0,0 +1,76 @@
+<style>
+    .content.container {
+        display: flex;
+        gap: 1em;
+        flex-wrap: wrap;
+    }
+
+    .content > a {
+        color: inherit;
+        text-transform: none;
+    }
+
+    .content .card {
+        flex: 1 0 21%;
+        flex-grow: 0;
+        cursor: default;
+    }
+
+    .content .card:hover {
+        background-color: #f0f0f0;
+    }
+
+    .card-body > * {
+        display: flex;
+        align-self: center;
+        justify-self: start;
+        margin: 0px;
+    }
+
+    .card-body p {
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        display: block;
+        overflow: hidden;
+    }
+
+    @media only screen and (max-width: 990px) {
+        .content .card {
+            flex: 1 0 45%;
+            flex-grow: 0;
+        }    
+    }
+
+    @media only screen and (max-width: 600px) {
+        .content .card {
+            flex: 1 0 100%;
+            flex-grow: 0;
+        }    
+    }
+</style>
+<div class="content container mt-3 pt-3">
+    <?php if ($parent !== false) : ?>
+        <a href="?p=<?php echo urlencode($parent) ?>" class="card">
+            <div class="card-body"><p><i class="fa fa-chevron-circle-left go-back mr-3"></i>Volver</p></div>
+        </a>
+    <?php endif; ?>
+    <?php
+        foreach($folders as $f) {
+            $img = $is_link ? 'icon-link_folder' : 'fa fa-folder-o';
+    ?>
+            <a href="?p=<?php echo urlencode(trim(FM_PATH . '/' . $f, '/')) ?>" class="card">
+                <div class="card-body"><p><i style="font-size: 1.5em;" class="<?php echo $img ?> mr-3"></i><?= $f ?></p></div>
+            </a>
+    <?php
+        }
+        foreach($files as $f) {
+            $img = $is_link ? 'fa fa-file-text-o' : Utils\Icons\fm_get_file_icon_class($path . '/' . $f);
+            $filelink = '?p=' . urlencode(FM_PATH) . '&amp;view=' . urlencode($f);
+    ?>
+        <a class="card" href="<?php echo $filelink ?>" title="<?php echo $f ?>">
+            <div class="card-body"><p><i style="font-size: 1.5em;" class="<?php echo $img ?> mr-3"></i><?= $f ?></p></div>
+        </a>
+    <?php
+        }
+    ?>
+</div>
